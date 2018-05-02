e44a189e0435693f7de9bfca7608c3258ab7c92f
DESKTOP-840UKFH\ccccc <ViktarKarpovich@yandex.by>
2018-05-02 15:04:37 +0300

    dp

--- DatePiker/js/render.js
+++
@@ -1,6 +0,0 @@
-function Render() {}
-
-Render.render = function (acсeptPlace, object, objectGenerator) {
-    var generatedObject = objectGenerator.generate(object);
-    acсeptPlace.appendChild(generatedObject);
-}
